a1a02630fad1cf1b1a2cc0e0dddb6bebbf3f8e31
“evelinecai” <r.t.cai@tilburguniversity.edu>
2024-10-08 15:36:55 +0200

    hope this works

--- src/data-preperation/merge_data.R
+++ src/data-preperation/merge_data.R
@@ -1,20 +1,11 @@
+# Load package
+library(tidyverse)
+
 # Read datasets from specific paths
 
-data1 <- read_csv("../../data/data1.csv")
-data2 <- read_csv("../../data/data2.csv")
-data3 <- read_csv("../../data/data3.csv")
-
-
-# This code filters data1 to include only the rows where the primaryProfession column contains the words "actor" or "actress".
-data1 <-  data1[grepl("actor|actress", data1$primaryProfession), ]
-data3 <- data3[grepl("movie", data3$titleType), ]
-
-
-# removal of unnecessary columns
-library(dplyr)
-cdata1 <- data1 %>% select(primaryName, primaryProfession, knownForTitles)
-cdata2 <- data2
-cdata3 <- data3 %>% select(tconst, primaryTitle, startYear, genres)
+cdata1 <- read_csv("../../data/cdata1.csv")
+cdata2 <- read_csv("../../data/cdata2.csv")
+cdata3 <- read_csv("../../data/cdata3.csv")
 
 library(data.table)
 
@@ -28,13 +19,19 @@ cdata1 <- cdata1[, .(primaryName, primaryProfession, tconst = unlist(strsplit(kn
 # Split 'genres' into separate rows
 cdata3 <- cdata3[, .(tconst, primaryTitle, startYear, genre = unlist(strsplit(genres, ","))), by = .I]
 
+
 # creating 1 dataset
-data4 <- left_join(cdata2, cdata3, by = "tconst") 
-data5 <- left_join(data4, cdata1, by = c("tconst"))
-```
+data4 <- left_join(cdata2, cdata3, by = "tconst")
+
+# Remove rows with NA in 'tconst' in data4
+data4 <- data4 %>%
+  filter(!is.na(tconst))
+
+data5 <- left_join(data4, cdata1, by = c("tconst"), relationship = "many-to-many")
+
 
 # delete NA's
 data <- na.omit(data5)
 
 # Transform all datasets into a CSV file and save it in the specified directory
-write_csv(data, "../../gen/data-preparation/temp/data.csv")
+write_csv(data, "../../data/data.csv")
